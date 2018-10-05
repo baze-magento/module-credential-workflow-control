490db1064d770d76ddb3dd61d21498cab9f27dad
Tess Daniel <niflthaena@gmail.com>
2018-10-05 06:17:38 +0000

    Make form postable

---
+++ Controller/Account/CreateLoginPost.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Baze\CredentialWorkflowControl\Controller\Account;
+
+use Baze\CredentialWorkflowControl\Observer\LoginInterceptCheck;
+use Magento\Customer\Model\AuthenticationInterface;
+use Magento\Customer\Model\Customer\Mapper;
+use Magento\Customer\Model\EmailNotificationInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\CustomerExtractor;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\InvalidEmailOrPasswordException;
+use Magento\Framework\Exception\State\UserLockedException;
+use Magento\Framework\Escaper;
+use Magento\Framework\Math\Random;
+
+/**
+ * Class EditPost
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class CreateLoginPost extends \Magento\Customer\Controller\AbstractAccount
+{
+    /**
+     * Form code for data extractor
+     */
+    const FORM_DATA_EXTRACTOR_CODE = 'customer_account_edit';
+
+    /**
+     * @var AccountManagementInterface
+     */
+    protected $customerAccountManagement;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+
+    /**
+     * @var Validator
+     */
+    protected $formKeyValidator;
+
+    /**
+     * @var CustomerExtractor
+     */
+    protected $customerExtractor;
+
+    /**
+     * @var Random
+     */
+    protected $mathRandom;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var \Magento\Customer\Model\EmailNotificationInterface
+     */
+    private $emailNotification;
+
+    /**
+     * @var AuthenticationInterface
+     */
+    private $authentication;
+
+    /**
+     * @var Mapper
+     */
+    private $customerMapper;
+
+    /** @var Escaper */
+    private $escaper;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param Validator $formKeyValidator
+     * @param CustomerExtractor $customerExtractor
+     * @param Random $mathRandom
+     * @param Escaper|null $escaper
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        AccountManagementInterface $customerAccountManagement,
+        CustomerRepositoryInterface $customerRepository,
+        Validator $formKeyValidator,
+        CustomerExtractor $customerExtractor,
+        Random $mathRandom,
+        Escaper $escaper = null
+    ) {
+        parent::__construct($context);
+        $this->session = $customerSession;
+        $this->customerAccountManagement = $customerAccountManagement;
+        $this->customerRepository = $customerRepository;
+        $this->formKeyValidator = $formKeyValidator;
+        $this->customerExtractor = $customerExtractor;
+        $this->mathRandom = $mathRandom;
+        $this->escaper = $escaper ?: ObjectManager::getInstance()->get(Escaper::class);
+    }
+
+    /**
+     * Get authentication
+     *
+     * @return AuthenticationInterface
+     */
+    private function getAuthentication()
+    {
+
+        if (!($this->authentication instanceof AuthenticationInterface)) {
+            return ObjectManager::getInstance()->get(
+                \Magento\Customer\Model\AuthenticationInterface::class
+            );
+        } else {
+            return $this->authentication;
+        }
+    }
+
+    /**
+     * Change customer email or password action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $validFormKey = $this->formKeyValidator->validate($this->getRequest());
+
+        if ($validFormKey && $this->getRequest()->isPost()) {
+            $currentCustomerDataObject = $this->getCustomerDataObject($this->session->getCustomerId());
+            $customerCandidateDataObject = $this->populateNewCustomerDataObject(
+                $this->_request,
+                $currentCustomerDataObject
+            );
+
+            try {
+                $isPasswordChanged = $this->changeCustomerPassword($currentCustomerDataObject->getEmail());
+
+                $customerCandidateDataObject->setCustomAttribute('password_change_required', '0');
+                $this->customerRepository->save($customerCandidateDataObject);
+                $this->messageManager->addSuccess(__('You saved your account information.'));
+                $this->session->setData(LoginInterceptCheck::SESSION_VAR_NAME, "0");
+                return $resultRedirect->setPath('customer/account');
+            } catch (InvalidEmailOrPasswordException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (UserLockedException $e) {
+                $message = __(
+                    'You did not sign in correctly or your account is temporarily disabled.'
+                );
+                $this->session->logout();
+                $this->session->start();
+                $this->messageManager->addError($message);
+                return $resultRedirect->setPath('customer/account/login');
+            } catch (InputException $e) {
+                $this->messageManager->addErrorMessage($this->escaper->escapeHtml($e->getMessage()));
+                foreach ($e->getErrors() as $error) {
+                    $this->messageManager->addErrorMessage($this->escaper->escapeHtml($error->getMessage()));
+                }
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('We can\'t save the customer.'));
+            }
+        }
+
+        return $resultRedirect->setPath('customer/account/createlogin');
+    }
+
+    /**
+     * Get customer data object
+     *
+     * @param int $customerId
+     *
+     * @return \Magento\Customer\Api\Data\CustomerInterface
+     */
+    private function getCustomerDataObject($customerId)
+    {
+        return $this->customerRepository->getById($customerId);
+    }
+
+    /**
+     * Create Data Transfer Object of customer candidate
+     *
+     * @param \Magento\Framework\App\RequestInterface $inputData
+     * @param \Magento\Customer\Api\Data\CustomerInterface $currentCustomerData
+     * @return \Magento\Customer\Api\Data\CustomerInterface
+     */
+    private function populateNewCustomerDataObject(
+        \Magento\Framework\App\RequestInterface $inputData,
+        \Magento\Customer\Api\Data\CustomerInterface $currentCustomerData
+    ) {
+        $inputData->setParam('firstname', $this->session->getCustomer()->getFirstname());
+        $inputData->setParam('lastname', $this->session->getCustomer()->getLastname());
+        
+        $attributeValues = $this->getCustomerMapper()->toFlatArray($currentCustomerData);
+        $customerDto = $this->customerExtractor->extract(
+            self::FORM_DATA_EXTRACTOR_CODE,
+            $inputData,
+            $attributeValues
+        );
+        $customerDto->setId($currentCustomerData->getId());
+        if (!$customerDto->getAddresses()) {
+            $customerDto->setAddresses($currentCustomerData->getAddresses());
+        }
+
+        return $customerDto;
+    }
+
+    /**
+     * Change customer password
+     *
+     * @param string $email
+     * @return boolean
+     * @throws InvalidEmailOrPasswordException|InputException
+     */
+    protected function changeCustomerPassword($email)
+    {
+        $newPass = $this->getRequest()->getPost('password');
+        $confPass = $this->getRequest()->getPost('password_confirmation');
+        if ($newPass != $confPass) {
+            throw new InputException(__('Password confirmation doesn\'t match entered password.'));
+        }
+
+        $customer = $this->customerRepository->getById($this->session->getCustomerId());
+
+        $newPasswordToken = $this->mathRandom->getUniqueHash();
+        $this->customerAccountManagement->changeResetPasswordLinkToken($customer, $newPasswordToken);
+        
+        return $this->customerAccountManagement->resetPassword($email, $newPasswordToken, $newPass);
+    }
+
+    /**
+     * Get Customer Mapper instance
+     *
+     * @return Mapper
+     *
+     * @deprecated 100.1.3
+     */
+    private function getCustomerMapper()
+    {
+        if ($this->customerMapper === null) {
+            $this->customerMapper = ObjectManager::getInstance()->get(\Magento\Customer\Model\Customer\Mapper::class);
+        }
+        return $this->customerMapper;
+    }
+}
